393d2e58ef6a50ccd4d364edebfa0634cde23b48
lupe22 <lukas.pestalozzi@gmail.com>
2015-02-26 11:47:51 +0100

    DBMS extended

--- src/dbms/DataBaseManager.java
+++ src/dbms/DataBaseManager.java
@@ -1,4 +1,4 @@
-package DBMS;
+package dbms;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,9 +16,7 @@ public class DataBaseManager {
 	
 	public DataBaseManager(){
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/mariessa_pu", "mariessa_pu", "fellesprosjekt");
-			JPAQuery query = new JPAQuery(entityManager);
-		
+			Connection connection = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/mariessa_pu", "mariessa_pu", "fellesprosjekt");		
 		
 		
 		} catch (SQLException e) { 
@@ -30,7 +28,29 @@ public class DataBaseManager {
 	
 	
 	
-	
+	/**
+	 * prepares and executes an executes a SQL statement.
+	 * Example of use: ResultSet result = executeStatement(connection, "INSERT INTO User VALUES (name=?, age=?, gender=?)", "Olav", "12", "male");
+	 * @param con -> the connection to be used.
+	 * @param statement -> eg "INSERT INTO User VALUES (name=?, age=?, gender=?)"
+	 * @param arguments -> the string attributes represented by a ? in the statement string.
+	 * @return 
+	 */
+	public static ResultSet executeStatement(Connection con, String statement, String... arguments){
+		try {
+			PreparedStatement stmt = con.prepareStatement(statement);
+			
+			for (int i = 0; i < arguments.length; i++){
+				stmt.setObject(i+1, arguments[i]);
+			}
+			return stmt.executeQuery();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
 	
 	
 	
