cfe07a2d6e8579c6704d263fd02b163e2e03f54c
mariessa <mariessa@stud.ntnu.no>
2015-03-03 12:58:01 +0100

    req.hand

---
+++ src/server_client/RequestHandler.java
@@ -0,0 +1,17 @@
+package server_client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class RequestHandler {
+
+//	private BufferedReader server_input;
+//	private BufferedWriter console_output;
+//	
+//	public RequestHandler(BufferedWriter console_output, BufferedReader server_input) {
+//		this.console_output = console_output;
+//		this.server_input = server_input;
+//	}
+	
+	
+}
