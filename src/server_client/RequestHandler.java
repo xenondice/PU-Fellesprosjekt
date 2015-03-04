462fe2e87d7ea96553514222fceec1d5a695e137
mariessa <mariessa@stud.ntnu.no>
2015-03-04 11:35:47 +0100

    added something to rh

--- src/server_client/RequestHandler.java
+++ src/server_client/RequestHandler.java
@@ -2,47 +2,72 @@ package server_client;
 
 import calendar.Entry;
 import dbms.DataBaseManager;
+import exceptions.EntryDoesNotExistException;
+import exceptions.GroupDoesNotExistException;
 import exceptions.UserDoesNotExistException;
+import exceptions.UsernameAlreadyExistsException;
 import user.Group;
 import user.User;
 
 public class RequestHandler {
 
 	DataBaseManager dbm = new DataBaseManager();
-	User user;
 	
 	public void logIn(User user){
-	
 		// TODO
-		this.user =user;
 	}
 
+/* ===============
+ * User functions
+ *================*/ 
 	
-	public void createUser(String username, String name, String password, String salt, String email) throws UsernameAlreadyExistsException{
-		User user = new User(username, name, password, salt, email);
+	
+	public void createUser(User user) throws UsernameAlreadyExistsException{
 		dbm.addUser(user);
 	}
 	
-	public void editUser(User u){
-		// TODO
+	public void editUser(User u) throws UserDoesNotExistException{
+		dbm.editUser(u);
 	}
 	
 	
-	public void createEvent(int entryID , long startTime, long endTime, String location, String description, boolean isActive, String roomID) throws UserDoesNotExistException{
-		Entry event = new Entry(entryID, startTime, endTime,location, description, isActive, roomID);
-		dbm.addEntry(event, user.getUsername());
+	/* ===============
+	 * Entry functions
+	 *================*/ 
+	
+	public void createEntry(Entry e, User user) throws UserDoesNotExistException{
+		dbm.addEntry(e, user.getUsername());
 	}
 	
 	public void deleteEntry(Entry e){
-		// TODO
+		dbm.deleteEntry(e.getEntryID());
 	}
 	
-	public void editEntry(Entry e){
-		// TODO
+	public void editEntry(Entry e, User user) throws EntryDoesNotExistException{
+		dbm.editEntry(e, user.getUsername());
 	}
 	
-	public void createGroup(User[] users, String name){
-		Group group = new Group(users, name);
+	public void kickUserFromEntry(User admin, User user, Entry entry){
+		dbm.hideEvent(user.getUsername(), entry.getEntryID());
+	}
+	
+	public void kickGroupFromEntry(User admin, Group group, Entry entry) throws GroupDoesNotExistException{
+		dbm.hideEventGroup(group.getName(), entry.getEntryID());
+	}	
+	
+	public void inviteUserToEntry(User admin, User user, Entry entry){
+		dbm.inviteUser(admin.getUsername(), user.getUsername(), entry.getEntryID());
+	}
+	
+	public void inviteGroupToEntry(User admin, Group group, Entry entry){
+		dbm.inviteUser(admin.getUsername(), group.getName(), entry.getEntryID());
+	}
+	
+	/* ===============
+	 * Group functions
+	 *================*/ 
+	
+	public void createGroup(Group group){
 		dbm.addGroup(group);
 	}
 	
@@ -54,7 +79,20 @@ public class RequestHandler {
 		dbm.removeUserFromGroup(user.getUsername(), group.getName());
 	}
 	
-	public void createCalendar( User user){
-//		dbm.createCalendar(user.getUsername());
+	
+	/* ===============
+	 * 'Calendar' functions
+	 *================*/ 
+	
+	public void createCalendar( User user) throws UserDoesNotExistException{
+		dbm.createCalendar(user.getUsername());
+	}
+	
+	public void invitationAnswer(User u, Entry e, boolean answer){
+		if (answer == true){
+			dbm.going(u.getUsername(), e.getEntryID());
+		}else{
+			dbm.notGoing(u.getUsername(), e.getEntryID());
+		}
 	}
 }
