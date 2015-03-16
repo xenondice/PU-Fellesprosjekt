327da1aabbee6186916c6c851e0fa6eb4bcdfa11
mariessa <mariessa@stud.ntnu.no>
2015-03-16 15:34:41 +0100

    asfa

--- src/calendar/GenericCalendar.java
+++ src/calendar/GenericCalendar.java
@@ -63,35 +63,6 @@ public class GenericCalendar {
 		return day;
 	}
 
-	public static String getMonthName(int month) {
-		if (month == 1) {
-			return "January";
-		} else if (month == 2) {
-			return "March";
-		} else if (month == 3) {
-			return "April";
-		} else if (month == 4) {
-			return "May";
-		} else if (month == 5) {
-			return "June";
-		} else if (month == 6) {
-			return "July";
-		} else if (month == 7) {
-			return "August";
-		} else if (month == 8) {
-			return "February";
-		} else if (month == 9) {
-			return "February";
-		} else if (month == 10) {
-			return "February";
-		} else if (month == 11) {
-			return "February";
-		} else if (month == 12) {
-			return "February";
-		} else {
-			throw new IllegalArgumentException(month + " is not a valid month!");
-		}
-	}
 
 	public static String getDayName(int day) {
 		if (day == 1) {
