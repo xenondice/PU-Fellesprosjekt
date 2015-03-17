86e48d149b8c784ac7cbc162ce382f7d8b5fbcf3
eiropp <eiriko_5@hotmail.com>
2015-03-17 12:53:09 +0100

    AddGroupToGroup command

---
+++ src/server_client/commands/AddGroupToGroup.java
@@ -0,0 +1,62 @@
+package server_client.commands;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import exceptions.ForcedReturnException;
+import exceptions.GroupDoesNotExistException;
+import exceptions.HasNotTheRightsException;
+import exceptions.SessionExpiredException;
+import exceptions.UserDoesNotExistException;
+import server_client.Argument;
+import server_client.Command;
+import server_client.RequestHandler;
+import server_client.ServerClientHandler;
+import server_client.Argument.ArgumentType;
+
+public class AddGroupToGroup extends Command {
+	
+	@Override
+	public String get() {
+		return "add-group-to-group";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Add a group to a group.";
+	}
+
+	@Override
+	public String getManual() {
+		return ""
+				+ "In order to use this command, you must first have created a group.\n"
+				+ "You can do so with the create-group function. When a group is created\n"
+				+ "it's completly empty, If you want that group to contain yourself,\n"
+				+ "you must therefore add yourself to the group manually using this command.";
+	}
+
+	@Override
+	public Argument[][] getArguments() {
+		return new Argument[][]{
+			{
+				new Argument(false, "groupname of group to add", ArgumentType.text),
+				new Argument(false, "name of group", ArgumentType.text),
+			}
+		};
+	}
+
+	@Override
+	public String[] getExamples() {
+		return new String[0];
+	}
+
+	@Override
+	public String run(ServerClientHandler handler, List<Object> arguments, int syntax) throws IOException, TimeoutException, InterruptedException, ForcedReturnException, SessionExpiredException, HasNotTheRightsException, UserDoesNotExistException, GroupDoesNotExistException {
+		
+		if (RequestHandler.addGroupToGroup(handler.getUsername(),(String) arguments.get(0),(String) arguments.get(1)))
+			return "Group successfully added to group!";
+		else
+			return "Group couldn't be added!";
+	}
+}
