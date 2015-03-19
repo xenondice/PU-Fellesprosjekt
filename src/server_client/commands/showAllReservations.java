75345c306faaa40346e5b5a16d8cce0f6f563d6e
lupe22 <lukas.pestalozzi@gmail.com>
2015-03-19 11:30:58 +0100

    2 new commands

---
+++ src/server_client/commands/showAllReservations.java
@@ -0,0 +1,62 @@
+package server_client.commands;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import room_booking.RoomReservation;
+import server_client.Argument;
+import server_client.Argument.ArgumentType;
+import server_client.Command;
+import server_client.RequestHandler;
+import server_client.ServerClientHandler;
+import exceptions.ForcedReturnException;
+import exceptions.SessionExpiredException;
+
+public class showAllReservations extends Command{
+
+	@Override
+	public String get() {
+		return "show-room-reservations";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Shows all reservations for a given room.";
+	}
+
+	@Override
+	public String getManual() {
+		return getDescription();
+	}
+
+	@Override
+	public Argument[][] getArguments() {
+		return new Argument[][]{
+				{
+					new Argument(false, "the room Id ", ArgumentType.text)
+				}
+		};
+	}
+
+	@Override
+	public String[] getExamples() {
+		return new String[] {"'"+this.get()+"' R-205"};
+	}
+
+	@Override
+	public String run(ServerClientHandler handler, List<Object> arguments,
+			int sytax) throws IOException, TimeoutException,
+			InterruptedException, ForcedReturnException,
+			SessionExpiredException {
+		
+		String roomID = (String)arguments.get(0);
+		HashSet<RoomReservation> res = RequestHandler.getAllReservationsForRoom(roomID);
+		if(res == null || res.isEmpty()){
+			return "There are no reservations for this room ("+roomID+").";
+		}
+		return res.toString();
+	}
+
+}
---
+++ src/server_client/commands/showAllRooms.java
@@ -0,0 +1,69 @@
+package server_client.commands;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
+
+import room_booking.Room;
+import server_client.Argument;
+import server_client.Command;
+import server_client.RequestHandler;
+import server_client.ServerClientHandler;
+import exceptions.EntryDoesNotExistException;
+import exceptions.ForcedReturnException;
+import exceptions.GroupAlreadyExistsException;
+import exceptions.GroupDoesNotExistException;
+import exceptions.HasNotTheRightsException;
+import exceptions.InvitationDoesNotExistException;
+import exceptions.SessionExpiredException;
+import exceptions.UserDoesNotExistException;
+import exceptions.UserInGroupDoesNotExistsException;
+import exceptions.UsernameAlreadyExistsException;
+
+public class showAllRooms extends Command {
+
+	@Override
+	public String get() {
+		return "show-rooms";
+	}
+
+	@Override
+	public String getDescription() {
+		return "shows all existing rooms.";
+	}
+
+	@Override
+	public String getManual() {
+		return "To use this command just enter '"+this.get()+"' and a list of all existing rooms will be shown";
+	}
+
+	@Override
+	public Argument[][] getArguments() {
+		return new Argument[0][0];
+	}
+
+	@Override
+	public String[] getExamples() {
+		return new String[] {"There is only one example: '"+this.get()+"'"};
+	}
+
+	@Override
+	public String run(ServerClientHandler handler, List<Object> arguments,
+			int sytax) throws IOException, TimeoutException,
+			InterruptedException, ForcedReturnException,
+			SessionExpiredException {
+		
+		StringBuilder sb = new StringBuilder();
+		HashSet<Room> rooms = RequestHandler.getAllRooms();
+		sb.append("All existing Rooms: \n");
+		for(Room r: rooms){
+			sb.append("-> ");
+			sb.append(r.toString());
+			sb.append("\n");
+		}
+		sb.append("\n");
+		return sb.toString();
+	}
+
+}
